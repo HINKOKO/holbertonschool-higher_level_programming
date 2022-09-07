c4532c3c6047dedeeba4b7585c78f12676ebd195
root <piotr62@live.fr>
2022-09-07 09:45:47 -0700

    zek git add 101-remove_char_at.py

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    newstr = ""
-    for i in str:
-        if n >= 0:
-            newstr = str[:n] + str[n + 1:]
-            return newstr
-        else:
-            return str
+    if n >= 0:
+        newstr = str[:n] + str[n + 1:]
+        return newstr
+    else:
+        return str
