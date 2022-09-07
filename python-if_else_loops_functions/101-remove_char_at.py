42d784a607e557d2b87caa62a56ce17176ec7bf4
root <piotr62@live.fr>
2022-09-07 09:43:26 -0700

    'now I am ignoring the negative index

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -3,7 +3,7 @@ def remove_char_at(str, n):
     newstr = ""
     for i in str:
         if n >= 0:
-            newstr = str[0: n:] + str[n + 1::]
+            newstr = str[:n] + str[n + 1:]
+            return newstr
         else:
             return str
-        return newstr
