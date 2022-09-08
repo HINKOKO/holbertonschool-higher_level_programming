f7570e77cb1e20bb94b4e910624617371fb8d2cd
root <piotr62@live.fr>
2022-09-08 05:20:22 -0700

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+from ast import Num
 from sys import argv
 
 if __name__ == "__main__":
-    if (len(argv) < 2):
-        print("{} argument:".format(len(argv)))
+    n = len(argv)
+    if n == 1:
+        print("{} arguments.".format(n - 1))
+    elif n == 2:
+        print("{} argument:".format(n - 1))
     else:
-        print("{} arguments:".format(len(argv)))
-for i, arg in enumerate(argv):
-    print("{}: {}".format(i, arg))
+        print("{} arguments:".format(n - 1))
+
+for i in range(1, n):
+    print("{}: {}".format(i, argv[i]))
