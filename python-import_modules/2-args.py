a4eba4f7bade759bbe9fe5695757cc389dda896d
root <piotr62@live.fr>
2022-09-08 04:14:51 -0700

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,8 +3,8 @@ from sys import argv
 
 if __name__ == "__main__":
     if (len(argv) < 2):
-        print("Number of argument(s) {} argument:".format(len(argv)))
+        print("{} argument:".format(len(argv)))
     else:
-        print("Number of argument(s) {} arguments:".format(len(argv)))
+        print("{} arguments:".format(len(argv)))
 for i, arg in enumerate(argv):
     print("{}: {}".format(i, arg))
