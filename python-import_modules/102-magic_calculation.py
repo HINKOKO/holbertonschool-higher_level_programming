28f7c16468adacee917d565d3d59d36a883e0ecf
root <piotr62@live.fr>
2022-09-09 02:38:24 -0700

    Task magic

--- python-import_modules/102-magic_calculation.py
+++ python-import_modules/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
         return c
-        for i in range(4, 6):
-            c = add(c, i)
+    for i in range(4, 6):
+        c = add(c, i)
     else:
         return sub(a, b)
