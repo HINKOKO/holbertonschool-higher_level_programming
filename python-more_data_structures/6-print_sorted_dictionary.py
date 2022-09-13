a7a66f47ea7dda4c90e04eeb8ed9170218d4744f
root <piotr62@live.fr>
2022-09-13 01:32:23 -0700

    Should be better!

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-from typing import OrderedDict
-
-
 def print_sorted_dictionary(a_dictionary):
-    for key, value in sorted(a_dictionary.items()):
-        print("{}: {}".format(key, value))
+    for key in sorted(a_dictionary):
+        print("{}: {}".format(key, a_dictionary[key]))
