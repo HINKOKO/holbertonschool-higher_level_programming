2384e338d90484e95dd06b414e4fdac1e0b94041
root <piotr62@live.fr>
2022-09-13 01:22:44 -0700

    testing 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,4 @@ from typing import OrderedDict
 
 
 def print_sorted_dictionary(a_dictionary):
-    sdict = OrderedDict(sorted(a_dictionary.items()))
-    return sdict
+    return (sorted(a_dictionary))
