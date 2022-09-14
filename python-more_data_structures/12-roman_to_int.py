45e7a4a1ec4337c5f086cb2fdb146faa4e171cff
root <piotr62@live.fr>
2022-09-14 06:24:03 -0700

    Ave Caesar if string null or non-Roman-Glory

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    r = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
-    res = 0
-    for i in range(len(roman_string) - 1):
-        left = roman_string[i]
-        right = roman_string[i + 1]
-        if r[left] < r[right]:
-            res -= r[left]
-        else:
-            res += r[left]
+    if type(roman_string) != str or not roman_string:
+        return 0
+    else:
+        r = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+        res = 0
+        for i in range(len(roman_string) - 1):
+            left = roman_string[i]
+            right = roman_string[i + 1]
+            if r[left] < r[right]:
+                res -= r[left]
+            else:
+                res += r[left]
     res += r[roman_string[-1]]
     return res
