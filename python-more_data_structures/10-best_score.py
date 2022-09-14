857956a7f5ad91a46e04d17183da9a233e749cf9
root <piotr62@live.fr>
2022-09-14 02:10:32 -0700

    Task 10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max_value = 0
+    if a_dictionary:
+        max_value = max(a_dictionary, key=a_dictionary.get)
+        return max_value
+    return None
