b4e2710c20bfbd89e3a709ef108bfc34afe45931
root <piotr62@live.fr>
2022-09-14 06:02:11 -0700

    Task 100 fixin' case list empty

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    average = sum([i[0]*i[1] for i in my_list]) / \
-        (sum([i[1] for i in my_list]))
-    return average
+    if my_list:
+        average = sum([i[0]*i[1] for i in my_list]) / \
+            (sum([i[1] for i in my_list]))
+        return average
+    else:
+        return 0
