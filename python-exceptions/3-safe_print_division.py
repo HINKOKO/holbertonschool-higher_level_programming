a6ce39cd1f1fbe778a21e8817cb71816fdd62395
root <piotr62@live.fr>
2022-09-15 01:47:41 -0700

    fixin'

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    res = 0
     try:
         res = a / b
     except (ZeroDivisionError, TypeError):
-        return None
+        res = None
     finally:
         print("Inside result: {}".format(res))
     return res
