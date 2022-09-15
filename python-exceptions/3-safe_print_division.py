907583bb781297976a0f89d2e66aa36c56618363
root <piotr62@live.fr>
2022-09-15 01:43:28 -0700

    adding typerror to be neat

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,8 @@ def safe_print_division(a, b):
     res = 0
     try:
         res = a / b
-    except (ZeroDivisionError, ValueError):
+    except (ZeroDivisionError, TypeError):
         return None
     finally:
         print("Inside result: {}".format(res))
+    return res
