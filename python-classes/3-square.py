263626878263eea2999c4d705aee556d02b0dbf0
root <piotr62@live.fr>
2022-09-19 05:38:46 -0700

    Area 51

---
+++ python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module - 2
+Defines a class Square with private size
+Checks for size type and value and valid it
+"""
+
+
+class Square:
+    """
+    class Square
+    Instantiation with size
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes square
+        Attributes:
+            private one, __size, must be of type int
+            or greater than 0
+            """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        return int(self.__size ** 2)
