6ab857f100c06e9a6b008fc4beeaa8b3acca1bf5
root <piotr62@live.fr>
2022-09-19 05:31:51 -0700

    pushing 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class Square:
+    """
+    class Square
+    Instantiation with size
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes square
+        Attributes:
+            private one, __size, must be of type int
+            or greater than 0
+            """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._size = size
