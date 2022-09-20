75acbd03025672563851e032038858d9f09bc7e2
root <piotr62@live.fr>
2022-09-20 06:13:27 -0700

    testin my magic class

--- python-classes/103-magic_class.py
+++ python-classes/103-magic_class.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 from cmath import pi
+"""Magic Module for MagicClass"""
 
 
 class MagicClass:
+    """MagicClass
+    Defines a class for computing perimeter
+    """
+
     def __init__(self, radius):
+        """check for radius valid data"""
         if type(radius) is not int and \
                 type(radius) is not float:
             raise TypeError('radius must be a number')
