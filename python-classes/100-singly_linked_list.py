e8cb1f5170993b432428b83cc28fa902559a9c9e
root <piotr62@live.fr>
2022-09-20 06:55:10 -0700

    nothin' on screen ty to figure out why with checker

---
+++ python-classes/100-singly_linked_list.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""Module 7"""
+
+"""class Node defines a node of singly-linked list"""
+
+
+class Node:
+    """Instantiation with data and next_node"""
+
+    def __init__(self, data, next_node=None):
+        """Initializes Node"""
+        self.__data = data
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        """Getter - to retrieve data"""
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """Setter - to set value to data"""
+        if value is not int:
+            raise TypeError("data must be an integer")
+        else:
+            self.__data = value
+
+    @property
+    def next_node(self):
+        """Getter - to retrieve next_node"""
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """Setter - to set value to  next_node"""
+        if type(value) != Node:
+            raise TypeError("next_node must be a Node object")
+        else:
+            self.__next_node = value
+
+
+class SinglyLinkedList:
+    """Instantiation with __init__"""
+
+    def __init__(self):
+        self.__head = None
+
+    def print(self):
+        current = self.__head
+        while (current):
+            print(current.data)
+            current = current.next
+
+    def sorted_insert(self, value):
+        """
+        Inserts new node according to increasing order
+        """
+        new = Node(value)
+        if self.__head is None:
+            self.__head = new
+            return
+        elif self.__head.data > new.data:
+            self.__head = new
+            return
+        else:
+            tmp = self.__head
+            while new.data > tmp.data and tmp.next_node is not None:
+                tmp = tmp.next_node
+                new.next_node = tmp.next_node
+                tmp.next_node = new
+                return
