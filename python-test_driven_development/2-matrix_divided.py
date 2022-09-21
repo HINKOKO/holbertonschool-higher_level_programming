57ac694c5c964112c25e02b23cad0c0cf48fe9c2
root <piotr62@live.fr>
2022-09-21 05:40:06 -0700

    better version

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -10,30 +10,22 @@ def matrix_divided(matrix, div):
     -matrix must be a list of integers or floats
     -Each row of the matrix must be of the same size
     -div must be a a number and a positive one dude"""
-    error_int_float = "matrix must be a matrix (list of lists) of integers/floats"
-    if type(matrix) is not list or len(matrix) == 0 or len(matrix[0]) == 0:
-        raise TypeError(error_int_float)
-
-    elif div == 0:
-        raise ZeroDivisionError("division by zero")
-
-    elif not isinstance(div, (float, int)):
+    error_L_int_float = "matrix must be a matrix (list of lists) of integers/floats"
+    if type(matrix) is not list:
+        raise TypeError(error_L_int_float)
+    if not isinstance(div, (float, int)):
         raise TypeError("div must be a number")
-
-    else:
-        len_check = len(matrix[0])
-        new_matrix = []
-        for row in matrix:
-            if type(row) is not list:
-                raise TypeError(error_int_float)
-
-            if len(row) != len_check:
-                raise TypeError(
-                    "Each row of the matrix must have the same size")
-            new_cols = []
-            for i in row:
-                if not isinstance(i, (int, float)):
-                    raise TypeError(error_int_float)
-                new_cols.append(round(i/div, 2))
-            new_matrix.append(new_cols)
-        return new_matrix
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    len_checker = len(matrix[0])
+    new_matrix = []
+    for row in matrix:
+        if len(row) != len_checker:
+            raise TypeError("Each row of the matrix must have the same size")
+        new_cols = []
+        for i in row:
+            if not isinstance(i, (float, int)):
+                raise TypeError(error_L_int_float)
+            new_cols.append(round(i/div, 2))
+        new_matrix.append(new_cols)
+    return new_matrix
