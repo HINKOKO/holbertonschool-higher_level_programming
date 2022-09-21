1299de72865a398e2623d81f969c7d32f36bb662
root <piotr62@live.fr>
2022-09-21 07:40:21 -0700

    Task name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,9 +6,12 @@ Prints "My name is <first name> <last name>"
 
 
 def say_my_name(first_name, last_name=""):
+    """say_my_name
+    -Print the first and last name
+    -Both must be strings"""
     if type(first_name) is not str:
-        raise TypeError("{:s} must be a string".format(first_name))
+        raise TypeError("{:s} must be a string".format("first_name"))
     if type(last_name) is not str:
-        raise TypeError("{:s} must be a string".format(last_name))
+        raise TypeError("{:s} must be a string".format("last_name"))
     else:
         print("My name is {:s} {:s}".format(first_name, last_name))
