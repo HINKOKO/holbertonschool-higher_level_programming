311987ef433e6f449aebf044a4d932b4fd41946e
root <piotr62@live.fr>
2022-09-21 01:22:14 -0700

    okay git add 0-add_integer.py ./tests/

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,9 +10,9 @@ def add_integer(a, b=98):
     returns a + b
     After having checked a and b are integer or floats
     """
-    if not isinstance(a, int) and not isinstance(a, float):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    elif not isinstance(b, int) and not isinstance(b, float):
+    elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     else:
         return int(a) + int(b)
