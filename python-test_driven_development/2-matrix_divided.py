c4382b0fc90ea5630b5cdadfe8b1b96e6f51a0ae
root <piotr62@live.fr>
2022-09-21 05:45:42 -0700

    affinin

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -10,11 +10,10 @@ def matrix_divided(matrix, div):
     -matrix must be a list of integers or floats
     -Each row of the matrix must be of the same size
     -div must be a a number and a positive one dude"""
-    error_L_int_float = "matrix must be a matrix (list of lists)\
-         of integers/floats"
+    error = "matrix must be a matrix (list of lists) of integers/floats"
     if type(matrix) is not list or len(matrix) == 0 or \
             type(matrix[0]) is not list or len(matrix[0]) == 0:
-        raise TypeError(error_L_int_float)
+        raise TypeError(error)
     if not isinstance(div, (float, int)):
         raise TypeError("div must be a number")
     if div == 0:
@@ -27,7 +26,7 @@ def matrix_divided(matrix, div):
         new_cols = []
         for i in row:
             if not isinstance(i, (float, int)):
-                raise TypeError(error_L_int_float)
+                raise TypeError(error)
             new_cols.append(round(i/div, 2))
         new_matrix.append(new_cols)
     return new_matrix
