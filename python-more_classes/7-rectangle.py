4711e64242e08c78fbc2ccebccbb55f8ab4470b3
root <piotr62@live.fr>
2022-09-22 08:52:02 -0700

    Task 7

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -59,7 +59,8 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         else:
-            return ("\n".join(str(self.print_symbol) * self.width for x in range(self.height)))
+            return ("\n".join(str(self.print_symbol) * self.width
+                              for x in range(self.height)))
 
     def __repr__(self):
         """Prints the rectangle with pattern #"""
