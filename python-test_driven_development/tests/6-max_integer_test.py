ae2cefb06c321b0befa3bcf09e8af80894efc29d
root <piotr62@live.fr>
2022-09-22 02:08:27 -0700

    what you tell

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -6,4 +6,9 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_notinteger(self):
+        """test what happens when not integers are passed"""
         self.assertNotIsInstance(max_integer('B'), int)
+
+    def test_maxfound(self):
+        self.assertTrue(max_integer([1, 2, 3, 4]), 4)
+        self.assertTrue(max_integer([4, 3, 2, 1]), 4)
