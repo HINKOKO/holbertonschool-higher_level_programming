56bf9f23d760b34eda9e25055ada2986d806a0f0
root <piotr62@live.fr>
2022-09-26 07:23:03 -0700

    testin you

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""Module 101 - Low memory cost"""
+
+
 class LockedClass:
+    """ The basic usage of __slots__ 
+    is to save space in objects"""
     __slots__ = ['first_name']
