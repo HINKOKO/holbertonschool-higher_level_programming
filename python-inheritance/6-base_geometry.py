dddea056d42f7963b25dac5b04a7b7ecee0eb425
root <piotr62@live.fr>
2022-09-27 13:01:53 -0700

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module 5 -  Improve Geometry"""
+
+
+class BaseGeometry:
+    """class continuing"""
+
+    def area(self):
+        """aera method to be continued"""
+        raise Exception("area() is not implemented")
