4d77109c651556cd291c425b88081c19388d6fc3
root <piotr62@live.fr>
2022-09-28 01:22:14 -0700

    Task 101

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module 13 - Can I ? 
+Adds new attribute if possible, can I ?"""
+
+
+def add_attribute(obj, name, value):
+    """set attribute of name 'name'
+    with 'value' value if not present in dict"""
+    if ('__dict__' in dir(obj)):
+        setattr(obj, name, value)
+    else:
+        raise TypeError("can't add new attribute")
