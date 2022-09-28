657dcabb2c8c750dd9e8039da2de14f058c7f30a
root <piotr62@live.fr>
2022-09-28 02:20:48 -0700

    T>ask 2 appended

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,9 @@
 
 
 def append_write(filename="", text=""):
+    """Appends text to a filename
+    Doesn't overwrite it because mode 'a'
+    Create it if it doesn't exists"""
     with open(filename, mode="a", encoding="UTF8") as appender:
         appender.write(text)
         return len(text)
