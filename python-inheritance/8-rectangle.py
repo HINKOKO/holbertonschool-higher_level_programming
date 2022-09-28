61b6cec3186ddda4ac06568333545445f121624a
root <piotr62@live.fr>
2022-09-28 05:03:38 -0700

    removing unecessary self

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """initialize width and height after
         validating them with integer_validator"""
-        super().integer_validator("width", width)
+        self.integer_validator("width", width)
         self.__width = width
-        super().integer_validator("height", height)
+        self.integer_validator("height", height)
         self.__height = height
