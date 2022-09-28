3bd22dc091a4b275878a5917867cb5277a79c224
root <piotr62@live.fr>
2022-09-28 02:08:40 -0700

    Task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module - 1  Write to a friend
+or a file it's better"""
+
+
+def write_file(filename="", text=""):
+    """opens and write a string 'text'
+    to a file 'filename'"""
+    with open(filename, mode='w', encoding="UTF8") as writty:
+        writty.write(text)
+        return len(text)
