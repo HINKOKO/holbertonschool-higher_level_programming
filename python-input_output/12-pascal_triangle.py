bab180951b73f50a5efc44efa4a6dca51861b1e4
root <piotr62@live.fr>
2022-09-30 00:02:37 -0700

    improving bit

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -7,15 +7,15 @@ def pascal_triangle(n):
     of integers representing the
     Pascal's triangle of n"""
     if n <= 0:
-        return
+        return [[]]
     elif n == 1:
-        return [1]
+        return [[1]]
     else:
         pascal = []
         for row in range(n):
             newrow = [1]
             for col in range(1, row + 1):
-                newcell = newrow[col - 1] * ((row + 1 - col) / col)
+                newcell = newrow[col - 1] * float((row + 1 - col) / col)
                 newrow.append(int(newcell))
             pascal.append(newrow)
         return pascal
