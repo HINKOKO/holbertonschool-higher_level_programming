051fba1a8f6deab3f9d0c63a1084eefd86d0abb0
root <piotr62@live.fr>
2022-09-29 23:56:55 -0700

    Pascal you crazy boy

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module 12 - Pascal is on fire !"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists
+    of integers representing the
+    Pascal's triangle of n"""
+    if n <= 0:
+        return
+    elif n == 1:
+        return [1]
+    else:
+        pascal = []
+        for row in range(n):
+            newrow = [1]
+            for col in range(1, row + 1):
+                newcell = newrow[col - 1] * ((row + 1 - col) / col)
+                newrow.append(int(newcell))
+            pascal.append(newrow)
+        return pascal
