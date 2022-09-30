1ad5d8c569ac5455c17ad7b50b5b3e40507c6758
root <piotr62@live.fr>
2022-09-30 03:25:20 -0700

    yeeeah

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -10,7 +10,7 @@ def append_after(filename="", search_string="", new_string=""):
         2- add the lines which does'nt match
         3- if there's a match add the new_string after pattern
         4- overwrite the original file, for so use seek(0)
-        to set the File Handle at the beginning 
+        to set the File Handle at the beginning
         """
     with open(filename, mode="r+") as old:
         new_text = ""
