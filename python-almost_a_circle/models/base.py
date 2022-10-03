a66ac45168ec78b8ad340635c8561f058cd12b97
root <piotr62@live.fr>
2022-10-03 05:55:59 -0700

    come on tchuk

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """ 1 - Base class"""
 
+import os
+
 
 class Base:
-	"""Base class
-	with private class attribute
-	Instantiation with __init__ """
+    """Base class
+    with private class attribute
+    Instantiation with __init__ """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if id is not None:
+        """id set to id if not None"""
+        if id:
             self.id = id
         else:
-            __nb_objects += 1
-            self.id = id
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
