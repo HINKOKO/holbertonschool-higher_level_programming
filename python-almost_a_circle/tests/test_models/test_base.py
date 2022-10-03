2ed7740a7a86831b8f9618d1e7592e315810560e
root <piotr62@live.fr>
2022-10-03 12:52:18 -0700

    wouuuha the error was hidden

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -3,20 +3,7 @@
 I'm really understanin nothin a t lal
 """
 import unittest
-import pycodestyle
 from models.base import Base
-
-
-class TestStyleFormat(unittest.TestCase):
-    """check for pycodestyle"""
-
-    def test_style_conformance(self):
-        """Test that we match pycodestyle"""
-        style = pycodestyle.StyleGuide(quiet=True)
-        result = style.check_files(
-            ['models/base.py', 'tests/test_models/test_base.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings)")
 
 
 class TestBase(unittest.TestCase):
@@ -33,3 +20,9 @@ class TestBase(unittest.TestCase):
         b2 = Base(523)
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 523)
+
+    def test_ty_inst(self):
+        """test for type and instance"""
+        bibi = Base()
+        self.assertEqual(type(bibi), Base)
+        self.assertTrue(isinstance(bibi, Base))
