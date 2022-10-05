55cb90ab92fc685bcff4be797e8c57a55531ac6f
root <piotr62@live.fr>
2022-10-05 02:50:28 -0700

    Moving forward

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ 1 - Base class"""
 import json
+import os.path
 
 
 class Base:
@@ -24,3 +25,44 @@ class Base:
         if list_dictionaries is None:
             return ("[]")
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """class method - writes the JSON string rep
+        of list_obj to a file"""
+        record = []
+        if list_objs is not None:
+            for inst in list_objs:
+                record.append(cls.to_dictionary(inst))
+            fname = cls.__name__ + ".json"
+            with open(fname, "w") as f:
+                f.write(cls.to_json_string(record))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """static method-returns the list of JSON string
+        representation ``json_strings``"""
+        json_list = "[]"
+        if json_string is not None or len(json_string) != 0:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """class method- returns an instance with all
+        attributes already set"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """returns list of instances"""
+        record_name = cls.__name__ + ".json"
+        book = []
+        if os.path.exists(record_name):
+            with open(record_name, "r") as record:
+            book.append(cls.create())
+        return book
