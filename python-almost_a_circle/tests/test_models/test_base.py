a1ee81e03a317f7131df0eb61cb87a0d92b320e2
root <piotr62@live.fr>
2022-10-05 12:03:50 -0700

    Bangooo

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -33,7 +33,24 @@ class TestBase(unittest.TestCase):
         self.assertTrue(isinstance(b, Base))
         self.assertEqual(type(b), Base)
 
-    def test_json_str_empty(self):
+    def test_to_json_str_empty(self):
         """test if empty list produce what expected"""
         bjson = Base.to_json_string([])
         self.assertEqual(bjson, '[]')
+    def test_to_json_content(self):
+        """test if list of dict is converted to JSON content"""
+        bjson = Base.to_json_string({"banjo": 500, "cithare":2560})
+        self.assertTrue(type(bjson), str)
+        self.assertEqual(bjson, '{"banjo": 500, "cithare": 2560}')
+    def test_from_json_str(self):
+        """test if JSON is converted to list
+        case empty, None, and correct JSON provided"""
+        blist = Base.from_json_string('[]')
+        blist_1 = Base.from_json_string(None)
+        blist_2 = Base.from_json_string('[{"Peach": 2.70, "Orange": 1.60}]')
+        self.assertEqual(blist, [])
+        self.assertEqual(blist_1, [])
+        self.assertEqual(blist_2, [{'Peach': 2.7, 'Orange': 1.6}])
+        self.assertTrue(type(blist), list)
+        self.assertTrue(type(blist_2), list)
+
