32a2d02a587f853dbded9f4d0eeef1cde5c6a47a
root <piotr62@live.fr>
2022-10-05 11:22:59 -0700

    come on fabilien!

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """unittests stuff to test the stuff
-I'm really understanin nothin a t lal
+I'm really understanding nothing at all
 """
 import unittest
 from models.base import Base
@@ -9,13 +9,16 @@ from models.base import Base
 class TestBase(unittest.TestCase):
     """class for testing Base class"""
 
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
     def test_id(self):
         """Test that ID associated exists and match
         with new instances created"""
         b1 = Base()
         b2 = Base(523)
         b3 = Base(62)
-        b4 = Base(None)
+        b4 = Base()
         b5 = Base()
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 523)
@@ -23,13 +26,8 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b4.id, 2)
         self.assertEqual(b5.id, 3)
 
-    def test_ty_inst(self):
-        """test for type and instance"""
-        bibi = Base()
-        self.assertEqual(type(bibi), Base)
-        self.assertTrue(isinstance(bibi, Base))
-
-    def json_(self):
-        """test that to_json_string empty exists"""
-        bjson = Base.to_json_string([])
-        self.assertEqual(bjson, '[]')
+    def test_type(self):
+        """test type and instance"""
+        b = Base()
+        self.assertTrue(isinstance(b, Base))
+        self.assertEqual(type(b), Base)
