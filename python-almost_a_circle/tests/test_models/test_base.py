3167f7e4e2bf0c156b84f4253dc00cf4cb55f75e
root <piotr62@live.fr>
2022-10-05 10:55:03 -0700

    trying to test the test

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -9,20 +9,27 @@ from models.base import Base
 class TestBase(unittest.TestCase):
     """class for testing Base class"""
 
-    def setUp(self):
-        """init our class"""
-        Base._Base__nb_objects = 0
-
     def test_id(self):
         """Test that ID associated exists and match
         with new instances created"""
         b1 = Base()
         b2 = Base(523)
+        b3 = Base(62)
+        b4 = Base(None)
+        b5 = Base()
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 523)
+        self.assertEqual(b3.id, 62)
+        self.assertEqual(b4.id, 2)
+        self.assertEqual(b5.id, 3)
 
     def test_ty_inst(self):
         """test for type and instance"""
         bibi = Base()
         self.assertEqual(type(bibi), Base)
         self.assertTrue(isinstance(bibi, Base))
+
+    def json_(self):
+        """test that to_json_string empty exists"""
+        bjson = Base.to_json_string([])
+        self.assertEqual(bjson, '[]')
