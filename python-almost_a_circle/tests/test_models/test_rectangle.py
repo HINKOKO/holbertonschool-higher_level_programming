fb71594da5deb29a435d49c9ba7c3b66ea5cf0dd
root <piotr62@live.fr>
2022-10-07 03:56:26 -0700

    addin som etests

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for testing Rectangle class"""
 import unittest
+from models.base import Base
 from models.rectangle import Rectangle
 
 
@@ -12,13 +13,14 @@ def tearDownModule():
 class TestBase(unittest.TestCase):
 	"""Basic test for rectangle module"""
 	
-	def test_rect_various_try(self):
+	def test_various_instances(self):
 		"""Tests with 2, 3, 4 regular args passed
 		Then with non valid type or negative integers"""
 		r1 = Rectangle(10, 1)
 		r2 = Rectangle(10, 2, 0, 0, 12)
 		self.assertEqual(r1.id, 3)
 		self.assertEqual(r2.id, 12)
+		self.assertEqual(r2._Base__nb_objects, 3)
 
 		with self.assertRaises(TypeError):
 			r3 = Rectangle("5", 6)
