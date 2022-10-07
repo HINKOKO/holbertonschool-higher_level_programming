f6c7845ffd12f0cc501938c885ba7668feeb2a7e
root <piotr62@live.fr>
2022-10-07 06:10:14 -0700

    is it okay

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,11 +5,9 @@ import os
 from models.base import Base
 from models.rectangle import Rectangle
 
-Base._Base__nb_objects = 0
-
 class TestRectangle(unittest.TestCase):
     """Basic test for rectangle module"""
-    def setUp(self):
+    def tearDown(self):
         Base._Base__nb_objects = 0
         self.assertEqual(Base._Base__nb_objects, 0)
     
@@ -17,23 +15,16 @@ class TestRectangle(unittest.TestCase):
         """Tests with 2, 3, 4 regular args passed
         Then with non valid type or negative integers"""
 
-        r1 = Rectangle(4, 5)
-        r2 = Rectangle(5, 6, 7)
-        with self.assertRaises(TypeError):
-            r5 = Rectangle("5", 6)
-            r6 = Rectangle("string")
-            r7 = Rectangle(6, 7, "1")
-            r8 = Rectangle(6, 5, 8, "9")
-            r9 = Rectangle(-5, 5)
-            r10 = Rectangle()
-            r11 = Rectangle(0, 8)
+    def test_valid(self):
+        """fuction that test for good assignment of differents value"""
+        r1 = Rectangle(10, 2, 34, 121, 45027310974)
+        self.assertEqual(r1.id, 45027310974)
+        self.assertEqual(r1.width, 10)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 34)
+        self.assertEqual(r1.y, 121)
         
-        self.assertEqual(r1.id, 1)
-        self.assertEqual(r1._Base__nb_objects, 2)
-        self.assertEqual(r2.id, 2)
-        self.assertEqual(r2._Base__nb_objects, 2)
         
-
     def test_area(self):
         """test that area exists when
         right arguments provided"""
