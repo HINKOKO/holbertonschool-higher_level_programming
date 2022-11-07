6be988d764a1680e30c47f47a09a53e5226ad82b
root <piotr62@live.fr>
2022-11-07 00:41:35 -0800

    Create User with password

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 -- Script that creates a new user [if not exists already]
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
