3aa689d8bd0b4f6f9312be66920daba8689790d0
root <piotr62@live.fr>
2022-11-07 06:26:54 -0800

    Task 9 JOIN POWER

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- Script that list all cities contained in database hbtn_0d_2
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY id ASC;
