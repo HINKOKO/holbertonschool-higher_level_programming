4d94aa4f5d23076f44db6f3248a6565472d3de4d
root <piotr62@live.fr>
2022-11-07 03:06:00 -0800

    Subquery == Sexy

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 -- Script that list all the cities of California in database hbtn_0d_usa using subqueries technic
 SELECT id, name
 FROM cities
-WHERE id IN (
+WHERE state_id IN (
 	SELECT id
 	FROM states
 	WHERE name='California'
