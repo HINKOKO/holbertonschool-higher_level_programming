a414b362a8ef35dab390bf417bfa6cf8d6739c34
root <piotr62@live.fr>
2022-11-08 08:30:37 -0800

    better version

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -12,9 +12,10 @@ if "__main__" == __name__:
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user="root",
+        passwd="root",
+        db="hbtn_0e_0_usa",
+        charset="utf8"
     )
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
