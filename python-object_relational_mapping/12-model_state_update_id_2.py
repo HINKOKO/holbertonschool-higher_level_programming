c6ec733915adb08f4a248e577f9035efbb949403
root <piotr62@live.fr>
2022-11-09 05:30:06 -0800

    update method thanks to tutorialspoint

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Start Adding and Updating objects
+Script that changes the name of a `State`object
+in our database
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.query(State).filter(State.id == 2).update(
+        {State.name: "New Mexico"})
+    session.commit()
+    session.rollback()
+    session.close()
