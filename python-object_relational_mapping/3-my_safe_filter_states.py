09b07a84a4445fdd4c2c8af797cfe45079d210b0
root <piotr62@live.fr>
2022-11-08 23:16:28 -0800

    trying and hoping to be safe and sound

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+== Safe version / prevents SQL Injection == 
+Script to list all states
+from database hbtn_0e_0usa
+where name of states matches an argument of command line
+
+"""
+
+import MySQLdb
+import sys
+
+if "__main__" == __name__:
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        passwd="root",
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = db.cursor()
+    cur.execute(
+        """SELECT * FROM states WHERE name LIKE '%s' \
+            ORDER BY states.id ASC""" % sys.argv[4]
+    )
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1] == sys.argv[4]:
+            print(row)
+    cur.close()
+    db.close()
