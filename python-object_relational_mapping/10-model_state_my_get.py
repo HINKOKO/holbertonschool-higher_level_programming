c62c576076ef589f6cf3769e6e1e5178bf6b9e06
root <piotr62@live.fr>
2022-11-09 04:55:21 -0800

    class task 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Start link class to table in database with sqlalchemy
+Script that prints the `State` Object with the 'name' passed
+as argument in command line.
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for wanted_id in session.query(State).filter(State.name == sys.argv[4]):
+        if wanted_id:
+            print("{:d}".format(wanted_id.id))
+        else:
+            print("Not found")
+    session.close()
