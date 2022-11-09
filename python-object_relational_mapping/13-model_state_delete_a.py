e2e358061e68f7aadcfbea605569e47f6da51b79
root <piotr62@live.fr>
2022-11-09 05:49:06 -0800

    All State Objects names containing '%a%' were deleted Boss

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Start Adding and Updating objects
+Script that deletes all `State` objects with name
+containing letter 'a' from our database born in
+the USA
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.query(State).filter(State.name.like('%a%')).\
+        delete(synchronize_session='fetch')
+    session.commit()
+    session.close()
