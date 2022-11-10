ff211c6a31d4607f7d721dcb7567be9f17ed59ef
root <piotr62@live.fr>
2022-11-10 08:33:32 -0800

    L'énoncé est dispendieux d'un détail non aggrégé sur le consulat des fiduciers!

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- Script to list all shows by their rating
-SELECT tv_shows.title, SUM(rate) AS `rating sum`
+SELECT tv_shows.title, SUM(rate) AS `rating`
 FROM tv_shows
 INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
-ORDER BY `rating sum` DESC;
+ORDER BY `rating` DESC;
