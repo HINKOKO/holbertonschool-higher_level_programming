6b59e2bf29d8c065965d429bd8dc2a2dea5024b6
root <piotr62@live.fr>
2022-12-05 07:07:39 -0800

    adding new methods to play with rectangle

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const y = 'X';
+    for (let i = 1; i <= this.height; i++) {
+      console.log(y.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
