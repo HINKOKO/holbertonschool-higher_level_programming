e3296e88b83aefb7fa6756dc874002f2d889e809
root <piotr62@live.fr>
2022-12-05 22:55:37 -0800

    >Task 8 following instructions

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,18 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return (list.reverse());
+  return list.map(list.pop, [...list]);
 };
+
+// # another method to do it
+// return list.map((item, idx) => list[list.length - 1 - idx]);
+
+// # or do it this way ==>
+// ---------------------
+// use const instead of let ,because let allows datatypes to be changed
+// behavior we probably don't want with an array
+//   const revList = []; ([] or new Array())
+//   for (let i = list.length - 1; i >= 0; i--) {
+//     revList.push(list[i]);
+//   }
+//   return revList;
+// };
