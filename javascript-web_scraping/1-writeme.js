69553a3b4be05d7d60c2b9f078f3f71e273e3936
root <piotr62@live.fr>
2022-12-12 00:24:59 -0800

    Task 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fs = require('fs');
+const content = process.argv[3];
+const filepath = process.argv[2];
+
+fs.writeFile(filepath, content, 'utf8', err => {
+  if (err) {
+    console.error(err);
+  }
+});
