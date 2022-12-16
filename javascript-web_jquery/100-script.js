0048d9d0356a41c51c4b0e5b85b2177c22aa53cc
root <piotr62@live.fr>
2022-12-16 00:06:52 -0800

    dealind with DOMContentLoaded

---
+++ javascript-web_jquery/100-script.js
@@ -0,0 +1,9 @@
+// DOMContentLoaded event fires when the initial HTML document has been completely loaded
+// and parsed without waiting for stylesheet, images, scripts to finish loading
+// basically, it's the goal of this advanced task here, to make us deal with this event
+// DOMContentLoaded may fire before the script has a chance to run, so wise to check before
+// by adding here our addEventListener on DOMContentLoaded
+
+document.addEventListener('DOMContentLoaded', e => {
+  document.querySelector('header').style.color = '#FF0000';
+});
