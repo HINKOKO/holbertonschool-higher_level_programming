d755a0b2d60bb4e807c7f19a0df78d83541b1e00
root <piotr62@live.fr>
2022-12-16 00:39:56 -0800

    Sorry Nehemie, but jQuery is more finger-keyboard-friendly thant Vanilla !

---
+++ javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('#add_item').bind('click', function () {
+    $('.my_list').append('<li>Item</li>');
+  });
+  $('#remove_item').bind('click', function () {
+    $('.my_list li:last-child').remove();
+  });
+  $('#clear_list').bind('click', function () {
+    $('.my_list').empty();
+  });
+});
